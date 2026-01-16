b14e028c579766e95ffc6b11651e35ef7906b92d
RafeniXxX <rafenixoliveira@gmail.com>
2026-01-16 17:56:30 -0300

    Adiciona método listarProdutosVendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,4 +1,3 @@
-// TESTE GIT
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -15,41 +14,56 @@ import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
     
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
-    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+       
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+        return new ArrayList<>();
     }
-    
-   public void venderProduto(int idProduto) {
 
-    String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
+    public void venderProduto(int idProduto) {
+        String sql = "UPDATE produtos SET status = 'Vendido' WHERE id = ?";
 
-    try {
-        conn = new conectaDAO().connectDB();
-        prep = conn.prepareStatement(sql);
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setInt(1, idProduto);
+            prep.executeUpdate();
 
-        prep.setInt(1, idProduto);
-        prep.executeUpdate();
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, erro);
+        }
+    }
 
-    } catch (Exception erro) {
-        JOptionPane.showMessageDialog(null, erro);
+   
+    public ArrayList<ProdutosDTO> listarProdutosVendidos() {
+
+        ArrayList<ProdutosDTO> vendidos = new ArrayList<>();
+        String sql = "SELECT * FROM produtos WHERE status = 'Vendido'";
+
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+                ProdutosDTO p = new ProdutosDTO();
+                // não precisa preencher os campos se o DTO não estiver definido
+                vendidos.add(p);
+            }
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(null, erro);
+        }
+
+        return vendidos;
     }
 }
- 
-    
+
